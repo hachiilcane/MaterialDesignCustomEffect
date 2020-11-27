a4179cc304f40b27a9232a5da2f5b7f3057a86cb
hachiilcane <hachi.il.cane@gmail.com>
2020-11-27 23:55:18 +0900

    Add a listbox and  a textbox which binds to the listbox's selection.

--- MaterialDesignCustomEffect/ViewModels/ComboBoxCombinationViewModel.cs
+++ MaterialDesignCustomEffect/ViewModels/ComboBoxCombinationViewModel.cs
@@ -8,9 +8,25 @@ namespace MaterialDesignCustomEffect.ViewModels
 {
     public class ComboBoxCombinationViewModel : BindableBase
     {
+        public List<string> Fruits { get; }
+
+        private string _selectedFruit;
+        public string SelectedFruit
+        {
+            get { return _selectedFruit; }
+            set { SetProperty(ref _selectedFruit, value); }
+        }
+
         public ComboBoxCombinationViewModel()
         {
-
+            Fruits = new List<string>()
+            {
+                "Apple",
+                "Banana",
+                "Grape",
+                "Lemon",
+                "Orange"
+            };
         }
     }
 }
