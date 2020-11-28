152023a790ac8ff418fb5f73ceb18827b3fbdfe5
hachiilcane <hachi.il.cane@gmail.com>
2020-11-28 12:47:07 +0900

    Give a dummy adorner through Behavior to TextBox.

---
+++ MaterialDesignCustomEffect/Views/SelectedEffectBehavior.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace MaterialDesignCustomEffect.Views
+{
+    public class SelectedEffectBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.TextChanged += AssociatedObject_TextChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            AssociatedObject.TextChanged -= AssociatedObject_TextChanged;
+        }
+
+        private void AssociatedObject_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var myAdornerLayer = AdornerLayer.GetAdornerLayer(AssociatedObject);
+            myAdornerLayer.Add(new RippleAdorner(AssociatedObject));
+        }
+    }
+}
