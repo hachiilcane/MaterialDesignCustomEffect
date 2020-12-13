5267ab96b1a236e49bc5c692cb2a8480e076bcf7
hachiilcane <hachi.il.cane@gmail.com>
2020-12-14 00:57:59 +0900

    Change the way of control of ripple.
    The way of using events has a side effect. It doesn't work well when using keyboard.

--- MaterialDesignCustomEffect/Views/ExecutableRipple.cs
+++ MaterialDesignCustomEffect/Views/ExecutableRipple.cs
@@ -2,32 +2,52 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace MaterialDesignCustomEffect.Views
 {
     public class ExecutableRipple : Ripple
     {
+        private DispatcherTimer _timer;
+        private PropertyInfo _rippleXProperty;
+        private PropertyInfo _rippleYProperty;
+
         /// <summary>
         /// Make a ripple forcely.
         /// </summary>
         public void DoRipple()
         {
-            // if IsCentered is set to false, the exception occurs in OnPreviewMouseLeftButtonDown().
-            RippleAssist.SetIsCentered(this, true);
-
-            // raise Down and Up event by force.
-            OnPreviewMouseLeftButtonDown(null);
-
-            var args = new MouseButtonEventArgs(Mouse.PrimaryDevice, 0, MouseButton.Left)
+            if (_timer == null)
             {
-                RoutedEvent = PreviewMouseLeftButtonUpEvent
-            };
+                _timer = new DispatcherTimer();
+                _timer.Interval = new TimeSpan(0, 0, 0, 0, 50);
+                _timer.Tick += _timer_Tick;
 
-            RaiseEvent(args);
+                _rippleXProperty = typeof(Ripple).GetProperty(nameof(RippleX), BindingFlags.Instance | BindingFlags.Public);
+                _rippleYProperty = typeof(Ripple).GetProperty(nameof(RippleY), BindingFlags.Instance | BindingFlags.Public);
+            }
+
+            // if RippleX and RippleY are not set, the center of ripple is at right-bottom corner.
+            _rippleXProperty.SetValue(this, ActualWidth / 2 - RippleSize / 2);
+            _rippleYProperty.SetValue(this, ActualHeight / 2 - RippleSize / 2);
+
+            VisualStateManager.GoToState(this, TemplateStateNormal, false);
+            VisualStateManager.GoToState(this, TemplateStateMousePressed, true);
+
+            // For the purpose of getting a natural effect, add a interval of UI thread.
+            _timer.Start();
+        }
+
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            _timer.Stop();
+
+            VisualStateManager.GoToState(this, TemplateStateNormal, true);
         }
     }
 }
